69916d04d68394552ff77425569ea104fb603a68
anurajbh <anurajbh@users.noreply.github.com>
2019-10-27 14:46:08 -0400

    Initial commit

---
+++ Assets/Scripts/ScreenManager.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreenManager : MonoBehaviour
+{
+    public GameObject TreeState;
+    public GameObject ReportState;
+    public GameObject CityState;
+    private void Awake()
+    {
+        DisplayScreen(TreeState);
+    }
+
+    private void DisplayScreen(GameObject ScreenState)
+    {
+        ScreenState.SetActive(true);
+    }
+    private void Update()
+    {
+        CheckForUserInput();
+    }
+
+    private void CheckForUserInput()
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Masters-Project-Anuraj-Bhatnagar
+ Master's Project for the Digital Media Program at Georgia Tech
