ae0cdc53e5ffb27fe9322f3b18359cf63ffd0811
anurajbh <anurajbh@users.noreply.github.com>
2019-11-04 10:04:19 -0500

    Initial Resource Modifying functionalities

---
+++ Assets/Scripts/GameResource.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameResource : MonoBehaviour
+{
+    public string name;//to-do: Make use of Resource names
+    public float value;
+    public float gain;
+    public float modifier;
+    public float timePeriod;
+    public float timer;
+    void Start()
+    {
+        value = 0f;
+        gain = 1f;
+        modifier = 1f;
+        timePeriod = 2f;
+        timer = 0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if (timer >= timePeriod)
+        {
+            value += (gain * modifier);
+            timer = 0f;
+        }
+        print(value);
+    }
+}
---
+++ Assets/Scripts/ResourceModification.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ResourceModification : MonoBehaviour
+{
+    public GameResource res;
+    public float use = 2f;
+    public Button button;
+    //to-do: Add other ways for Resource values to change
+    void Start()
+    {
+        button.onClick.AddListener(ResourceUsage);
+    }
+
+    private void ResourceUsage()
+    {
+        res.value /= use;
+    }
+
+    // Update is called once per frame
+    /*private void Update()
+    {
+        
+    }*/
+}
