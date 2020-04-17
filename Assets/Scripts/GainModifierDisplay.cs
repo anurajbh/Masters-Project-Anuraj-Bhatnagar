6e75c71a42df19663bb1c9f7a2ca0c7280650604
anurajbh <anurajbh@users.noreply.github.com>
2020-04-17 12:42:32 -0400

    added display of how nodes change the modifier values for each resource, changes to prefabs, addition of plus sign

---
+++ Assets/Scripts/GainModifierDisplay.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GainModifierDisplay : MonoBehaviour
+{
+    public GainModifier gainModifier;
+    public Text text;
+    void Awake()
+    {
+        gainModifier = GetComponentInParent<GainModifier>();
+        text = GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(gameObject.tag == "Energy")
+        {
+            text.text = gainModifier.modifyBy[0].ToString();
+        }
+        else if (gameObject.tag == "Wealth")
+        {
+            text.text = gainModifier.modifyBy[1].ToString();
+        }
+        else if (gameObject.tag == "Novel")
+        {
+            text.text = gainModifier.modifyBy[2].ToString();
+        }
+    }
+}
