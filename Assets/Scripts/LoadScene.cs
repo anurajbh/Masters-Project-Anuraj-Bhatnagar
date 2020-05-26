bf0cd62a90d0b3aa34d715222fabb1177b67b516
anurajbh <anurajbh@users.noreply.github.com>
2020-05-25 20:18:12 -0400

    added main menu and pause menu

---
+++ Assets/Scripts/LoadScene.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LoadScene : MonoBehaviour
+{
+    public int sceneIndex;
+    Scene scene;
+    Button button;
+    void Awake()
+    {
+        scene = SceneManager.GetActiveScene();
+        button = GetComponent<Button>();
+        button.onClick.AddListener(LoadTheScene);
+    }
+    void LoadTheScene()
+    {
+        SceneManager.LoadScene(sceneIndex);
+    }
+
+}
---
+++ Assets/Scripts/PauseTheGame.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PauseTheGame : MonoBehaviour
+{
+    public GameObject panel;
+
+    public Button pauseButton;
+
+    public Button playButton;
+    private bool paused = false;
+    float timeCurrent;
+
+    void Awake()
+    {
+        timeCurrent = Time.timeScale;
+        pauseButton.onClick.AddListener(PauseGame);
+        playButton.onClick.AddListener(Unpause);
+    }
+    void PauseGame()
+    {
+        paused = true;
+        
+    }
+
+    private void StopPlay()
+    {
+        //timeCurrent = Time.timeScale;
+        Time.timeScale = 0;
+        panel.SetActive(true);
+        pauseButton.interactable = false;
+    }
+
+    void Unpause()
+    {
+        paused = false;
+    }
+
+    private void ResumePlay()
+    {
+        Time.timeScale = timeCurrent;
+        panel.SetActive(false);
+        pauseButton.interactable = true;
+    }
+
+    private void Update()
+    {
+        if(paused)
+        {
+            StopPlay();
+        }
+        else if(!paused)
+        {
+            ResumePlay();
+        }
+    }
+}
