eaf2091b25c69ab86080c3917fce4efc61bb7ae7
anurajbh <anurajbh@users.noreply.github.com>
2020-09-23 12:04:59 -0400

    Credits added

---
+++ Assets/Scripts/LoadSceneOnExit.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadSceneOnExit : MonoBehaviour
+{
+    public string sceneName;
+    float timeElapsed;
+    public int timer;
+    [SerializeField] int seconds;
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetAxis("Cancel") != 0)
+        {
+            SceneManager.LoadScene(sceneName);
+        }
+        timeElapsed += Time.deltaTime;
+        seconds = (int)timeElapsed % 60;
+        //force a scene load of main menu on end credits
+        if (seconds >= timer)
+        {
+            SceneManager.LoadScene(sceneName);
+        }
+    }
+    private void Awake()
+    {
+        timeElapsed = 0;
+        seconds = 0;
+    }
+}
